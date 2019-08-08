689aabdede7ead4d3d30e516963e6dd7e74898bc
FelixWagner18 <mailfwagner@gmail.com>
2019-08-08 10:22:22 +0200

    updated all structure

---
+++ SpectrogramCNN.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+from math_utils import logMagStft, ffts
+
+
+# NN architecture (three conv and two fully connected layers)
+class SpectrogramCNN(nn.Module):
+    def __init__(self, device):
+        super(SpectrogramCNN, self).__init__()
+        self.first_conv = nn.Conv2d(1, 20, 5, 1)
+        self.second_conv = nn.Conv2d(20, 50, 5, 2)
+        self.third_conv = nn.Conv2d(50, 50, 5, 2)        
+        self.fc1 = nn.Linear(50*6*6, 500)
+        self.fc2 = nn.Linear(500, 10)
+        self.device = device
+
+    def forward(self, x):
+        n_fft = 510
+    
+        spectrograms = np.zeros((len(x), n_fft//2+1, int(2*64000/n_fft)+2))
+        for index, audio in enumerate(x.cpu().numpy()):
+            spectrograms[index] = logMagStft(audio, 16000, n_fft)
+        
+        x = torch.from_numpy(spectrograms[:, np.newaxis, :, :]).to(self.device).float()
+        
+        # x.size is (batch_size, 1, 256, 252)
+        x = F.relu(self.first_conv(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.second_conv(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.third_conv(x))
+        x = F.max_pool2d(x, 2, 2)
+        # x.size is (batch_size, 50, 6, 6)
+        x = x.view(-1, 6*6*50)
+
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
---
+++ train_utils.py
@@ -0,0 +1,44 @@
+import torch
+import numpy as np
+import torch.nn.functional as F
+import time
+
+
+# This function trains the model for one epoch
+def train(args, model, device, train_loader, optimizer, epoch, start_time):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tRuntime: {:.1f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item(), time.time() - start_time))
+                
+                
+# This function evaluates the model on the test data
+def test(args, model, device, test_loader, epoch):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        familyPredictions = np.zeros(len(test_loader.dataset), dtype=np.int)
+        for index, samples in enumerate(test_loader):
+            samples = samples.to(device)
+            familyPredictions[index*len(samples):(index+1)*len(samples)] = model(samples).max(1)[1].cpu() # get the index of the max log-probability
+    
+    familyPredictionStrings = trainDataset.transformInstrumentsFamilyToString(familyPredictions.astype(int))
+
+    with open('NN-submission-' +str(epoch)+'.csv', 'w', newline='') as writeFile:
+        fieldnames = ['Id', 'Expected']
+        writer = csv.DictWriter(writeFile, fieldnames=fieldnames, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        writer.writeheader()
+        for index in range(len(testDataset)):
+            writer.writerow({'Id': index, 'Expected': familyPredictionStrings[index]})
+    print('saved predictions')
