1dd8331e67361b3e0ac92d5a4fabf3203d66d936
FelixWagner18 <mailfwagner@gmail.com>
2019-08-08 11:20:55 +0200

    wip: fix bug in train utils

---
+++ evaluation_utils.py
@@ -0,0 +1,11 @@
+
+
+
+def get_mean_F1_score(path_submission, ):
+    # read csv file
+    
+    # for every class compare label to prediction and get tp_c,fp_c,tn_c,fn_c
+    
+        # calculate recall and precision
+        
+        # 
