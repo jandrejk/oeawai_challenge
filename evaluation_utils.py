6a95e1555d10be7f0f40a90444d83a496d24b90b
fewagner <mailfwagner@gmail.com>
2019-08-15 11:48:20 +0200

    loss measure and ResNet34

--- evaluation_utils.py
+++ evaluation_utils.py
@@ -1,5 +1,6 @@
 from train_utils import output_to_class
 from sklearn.metrics import f1_score
+import torch.nn.functional as F
 
 
 def get_mean_F1(model, validation_loader):
@@ -13,3 +14,24 @@ def get_mean_F1(model, validation_loader):
             mean_f1 += f1_score(target.detach().cpu().numpy(), output_to_class(output), average='micro') / len(validation_loader)
             
     return mean_f1
+
+
+def get_loss(model, validation_loader, device):
+    """
+    returns loss for a given dataloader
+    """
+    model.eval()
+    loss_epoch = 0
+    mean_f1 = 0
+    for (data, target) in validation_loader:
+        
+            data, target = data.to(device), target.to(device)
+        
+            output = model(data)
+            
+            loss = F.nll_loss(output, target)
+            
+            loss_epoch += loss.item() / len(validation_loader)
+            mean_f1 += f1_score(target.detach().cpu().numpy(), output_to_class(output), average='micro') / len(validation_loader)
+            
+    return loss_epoch, mean_f1
